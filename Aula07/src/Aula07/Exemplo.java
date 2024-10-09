47282061d23a366b6ba2d56f03c7b20558bddcef
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-10-09 20:20:22 -0300

    cr7

---
+++ Aula07/src/Aula07/Exemplo.java
@@ -0,0 +1,62 @@
+package Aula07;
+
+public class Exemplo {
+    
+    private String nome;
+    private String nacionalidade;
+    private int idade;
+    private float altura;
+    private float peso;
+    private char categoria;
+    private int vitorias;
+    private int derrotas;
+    private int empates;
+
+    public void apresentar() {
+
+    }
+
+    public void status() {
+
+    }
+
+    public void ganharLuta() {
+
+    }
+
+    public void perderLuta() {
+
+    }
+
+    public void empatarLuta() {
+        
+    }
+
+    private String getNome() {
+        return nome;
+    }
+    private void setNome(String n) {
+        this.nome = n;
+    }
+
+    private String getNacionalidade() {
+        return nacionalidade;
+    }
+    private void setNacionalidade(String nc) {
+        this.nacionalidade = nc;
+    }
+
+    private int getIdade() {
+        return idade;
+    }
+    private void setIdade(int i) {
+        this.idade = i;
+    }
+
+    private float getPeso() {
+        return peso;
+    }
+
+
+}
+
