2cce599dc3acb42bac526dd991f0284f2edf8b1e
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-10-09 18:48:18 -0300

    cr1

--- Aula06/src/Aula06/Controle.java
+++ Aula06/src/Aula06/Controle.java
@@ -1,37 +1,6 @@
 package Aula06;
-public class Controle {
-    private int volume;
-    private boolean ligado;
-    private boolean tocando;
+public class ControleRemoto {
+    public static void main(String[] args) throws Exception {
+        
+    }
 }
-
-public Controle() {
-    this.volume = 50;
-    this.ligado = false;
-    this.tocando = false;
-}
-
-private int getVolume() {
-    return volume;
-}
-
-private Boolean getLigado() {
-    return ligado;
-}
-
-private Boolean getTocando() {
-    return tocando;
-}
-
-private void setVolume(int v) {
-    this.volume = v;
-}
-
-private void setLigado(Boolean l) {
-    this.ligado = l;
-}
-
-private void setTocando(Boolean t) {
-    this.tocando = t;
-}
-
