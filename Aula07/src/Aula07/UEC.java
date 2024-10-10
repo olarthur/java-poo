179ddbffe792436678a9271b926488ef171dd491
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-10-10 18:09:09 -0300

    uec11

--- Aula07/src/Aula07/UEC.java
+++ Aula07/src/Aula07/UEC.java
@@ -1,8 +1,26 @@
 package Aula07;
 public class UEC {
     public static void main(String[] args) throws Exception {
-        Lutador l = new Lutador("PB", "France", 31, 1.75f, 68.9f, 11, 2, 1);
-        l.status();
+        Lutador l[] = new Lutador[6];
+
+        l[0] = new Lutador("PB", "France", 31, 1.75f, 68.9f, 11, 2, 1);
+        
+        l[1] = new Lutador("PS", "Brasil", 29, 1.68f, 57.8f, 14, 2, 3);
+
+        l[2] = new Lutador("SnS", "Germany", 27, 1.65f, 80.9f, 17, 1, 4);
+
+        l[3] = new Lutador("DC", "USA", 39, 1.93f, 81.6f, 12, 3, 3);
+
+        l[4] = new Lutador("Jonh", "Mexican", 23, 1.70f, 119.3f, 13, 3, 5);
+
+        l[5] = new Lutador("Shin", "Japanese", 24, 1.81f, 105.7f, 15, 4, 3);
+
+        l[0].apresentar();
+        l[1].apresentar();
+        l[2].apresentar();
+        l[3].apresentar();
+        l[4].apresentar();
+        l[5].apresentar();   
         
     }
 }
