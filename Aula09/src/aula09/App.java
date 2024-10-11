5442d6ecdc9db5304eeba4becf76bb8bbebef2d5
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-10-11 19:12:39 -0300

    hrc3

--- Aula09/src/aula09/App.java
+++ Aula09/src/aula09/App.java
@@ -2,5 +2,20 @@ package aula09;
 public class App {
     public static void main(String[] args) throws Exception {
         
+        Pessoa p1 = new Pessoa();
+        Aluno p2 = new Aluno();
+        Professor p3 = new Professor();
+        Funcionario p4 = new Funcionario();
+
+        p1.setNome("Aristoteles");
+        p2.setNome("Platao");
+        p3.setNome("Kafka");
+        p4.setNome("Bauman");
+
+        p2.setCurso("Filosofia");
+        p3.setSalario(2500);
+        p4.setSetor("Estoque");
+
+        
     }
 }
