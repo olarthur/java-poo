3703ba57f026de8336ea31c2c71c6cabe1eef06b
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-10-11 18:51:51 -0300

    hrc1

---
+++ Aula09/src/aula09/Aluno.java
@@ -0,0 +1,29 @@
+package aula09;
+
+public class Aluno extends Pessoa{
+
+    private int matr;
+    private String curso;
+
+
+    public void cancelatMatr() {
+
+    }
+
+
+    public int getMatr() {
+        return matr;
+    }
+
+    public void setMatr(int matr) {
+        this.matr = matr;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+}
