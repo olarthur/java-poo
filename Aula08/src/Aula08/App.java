88db3c3c3610bc2a693a706ba4f4e9679c99bcd7
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-10-11 16:59:03 -0300

    pss2

--- Aula08/src/Aula08/App.java
+++ Aula08/src/Aula08/App.java
@@ -2,6 +2,6 @@ package Aula08;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
     }
 }
--- Aula08/src/Aula08/Livro.java
+++ Aula08/src/Aula08/Livro.java
@@ -1,5 +1,62 @@
 package Aula08;
 
 public class Livro {
+    private String titulo;
+    private String autor;
+    private int totPaginas;
+    private boolean aberto;
+    private Pesooa leitor;
+    
 
+    public void detalhes() {
+
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+
+    public String getAutor() {
+        return autor;
+    }
+
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+
+    public int getTotPaginas() {
+        return totPaginas;
+    }
+
+    public void setTotPaginas(int totPaginas) {
+        this.totPaginas = totPaginas;
+    }
+
+
+    public boolean isAberto() {
+        return aberto;
+    }
+
+
+    public void setAberto(boolean aberto) {
+        this.aberto = aberto;
+    }
+
+
+    public Pesooa getLeitor() {
+        return leitor;
+    }
+
+
+    public void setLeitor(Pesooa leitor) {
+        this.leitor = leitor;
+    }
 }
