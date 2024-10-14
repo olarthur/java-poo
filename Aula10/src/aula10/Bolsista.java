84c493f2d71c37c4f552530d338c0c77151e1861
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-10-14 17:04:20 -0300

    hrc8

---
+++ Aula10/src/aula10/Bolsista.java
@@ -0,0 +1,24 @@
+package aula10;
+
+public class Bolsista extends Aluno{
+    private int bolsa;
+    
+
+    public void RenovarBolsa() {
+
+    }
+
+    public int getBolsa() {
+        return bolsa;
+    }
+
+    public void setBolsa(int bolsa) {
+        this.bolsa = bolsa;
+    }
+
+
+    @Override
+    public void PagarMensalidade() {
+
+    }
+}
