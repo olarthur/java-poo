d2dedcc900dfd61aae10261e9101362fc6ffce4f
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-10-14 19:26:17 -0300

    plmrf3

---
+++ Aula11/src/aula11/Ave.java
@@ -0,0 +1,21 @@
+package aula11;
+
+public class Ave {
+
+    private String corPena;
+
+    
+    public void fazerNinho() {
+
+    }
+
+
+    public String getCorPena() {
+        return corPena;
+    }
+
+    public void setCorPena(String corPena) {
+        this.corPena = corPena;
+    }
+
+}
---
+++ Aula11/src/aula11/Peixe.java
@@ -0,0 +1,20 @@
+package aula11;
+
+public class Peixe {
+
+    private String corEscama;
+
+
+    public void soltarBolha() {
+
+    }
+
+    
+    public String getCorEscama() {
+        return corEscama;
+    }
+
+    public void setCorEscama(String corEscama) {
+        this.corEscama = corEscama;
+    }
+}
