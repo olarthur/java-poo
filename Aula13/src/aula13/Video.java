919713063aeb42baa9b57c5c5bb7a70b8b91585f
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-10-16 19:12:05 -0300

    class13.1

--- Aula13/src/aula13/Video.java
+++ Aula13/src/aula13/Video.java
@@ -2,4 +2,42 @@ package aula13;
 
 public class Video {
 
+    private String titulo;
+    private String avaliacao;
+    private int views;
+    private int curtidas;
+
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(String avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public int getViews() {
+        return views;
+    }
+
+    public void setViews(int views) {
+        this.views = views;
+    }
+
+    public int getCurtidas() {
+        return curtidas;
+    }
+
+    public void setCurtidas(int curtidas) {
+        this.curtidas = curtidas;
+    }
+
 }
