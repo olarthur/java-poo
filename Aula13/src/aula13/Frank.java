97472930b6008443a5fb6391f12dd5de3a03f9c8
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-10-16 19:17:47 -0300

    13.4

--- Aula13/src/aula13/Frank.java
+++ Aula13/src/aula13/Frank.java
@@ -2,4 +2,27 @@ package aula13;
 
 public class Frank {
 
+    private String login;
+    private double totAssistido;
+
+
+    public void viuMaisUm() {
+
+    }
+
+    
+    public String getLogin() {
+        return login;
+    }
+    public void setLogin(String login) {
+        this.login = login;
+    }
+    public double getTotAssistido() {
+        return totAssistido;
+    }
+    public void setTotAssistido(double totAssistido) {
+        this.totAssistido = totAssistido;
+    }
+
+
 }
