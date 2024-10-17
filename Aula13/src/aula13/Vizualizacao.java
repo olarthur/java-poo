69c964620ce58438c926bb0fe48ea3882d83b6c0
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-10-17 18:44:50 -0300

    13.9

--- Aula13/src/aula13/Vizualizacao.java
+++ Aula13/src/aula13/Vizualizacao.java
@@ -15,10 +15,31 @@ public class Vizualizacao {
 
 
     public void avaliar() {
-
+        this.filme.setAvaliacao(5);
     }
 
-    
+
+    public void avaliar(int nota) {
+        this.filme.setAvaliacao(nota);
+    }
+
+
+    public void avaliar(float porc) {
+        int tot = 0;
+        if (porc <= 20) {
+            tot = 3;
+        } else if (porc <= 50) {
+            tot = 5;
+        } else if (porc <= 90) {
+            tot = 8;
+        } else {
+            tot = 10;
+        }
+
+        this.filme.setAvaliacao(tot);
+    }
+
+
     public Frank getEspectador() {
         return espectador;
     }
